170e6f3ddb2ada370dc342b90be3ac38bdffb12f
Daan Schipper <daancschipper@gmail.com>
2015-04-23 21:58:58 +0200

    improved checkstyle

--- src/main/java/snippet/main.java
+++ src/main/java/snippet/main.java
@@ -1,5 +1,13 @@
+package snippet;
+
+/**
+ * Main class.
+ */
 public class main {
 
+    /**
+     * Simple first method.
+     */
     public static void hello() {
         System.out.println("hello");
     }
