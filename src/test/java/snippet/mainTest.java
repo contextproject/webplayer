41e4ab158ac413d68da3977dc19a4b85d3422798
boomknuffelaar <daancschipper@gmail.com>
2015-04-28 11:20:31 +0200

    Update mainTest.java

--- src/test/java/snippet/mainTest.java
+++ src/test/java/snippet/mainTest.java
@@ -23,7 +23,7 @@ public class mainTest {
     @Test
     public void test1() {
         Snippet.hello();
-        assertEquals("hello\n", outContent.toString());
+        assertEquals("Hello World\n", outContent.toString());
     }
 
     @After
