9b5f688161c8d996b802fa723ea13b2105489519
Daan Schipper <daancschipper@gmail.com>
2015-05-06 10:15:15 +0200

    added database connecter to play framework

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import database.Database;
 import play.*;
 import play.mvc.*;
 
@@ -10,5 +11,10 @@ public class Application extends Controller {
     public static Result index() {
         return ok(index.render("Discovery","Welcome to the Soundcloud Discovery Page!"));
     }
+
+    public static void testDatabaseConnection() {
+        Database db = new Database();
+    }
+
 
 }
