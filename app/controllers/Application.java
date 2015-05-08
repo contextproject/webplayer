8b598bfcfc5f62087c0cb89be3cec11ff766a561
ahovanesyan <a.hovanesyan@gmail.com>
2015-05-08 04:25:46 +0200

    Added a connection between the webpage and java

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,12 +8,21 @@ import views.html.*;
 
 public class Application extends Controller {
 
+	private static RandomSnippet snpt;
+	
     public static Result index() {
-    	RandomSnippet snpt = new RandomSnippet();
-        return ok(index.render(snpt.getStart()));
+    	String url = "w.soundcloud.com/tracks/202852531";
+    	snpt = new RandomSnippet();
+        return ok(index.render(url, snpt.getStart()));
     }
 
     public static void testDatabaseConnection() {
-        Database db = new Database();
+        //Database db = new Database();
+    }
+    
+    public static Result getStartTime(String url) {
+    	RandomSnippet snpt = new RandomSnippet();
+    	String url2 = "w.soundcloud.com/tracks/" + url;
+        return ok(index.render(url2, snpt.getStart()));
     }
 }
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,107 +1,112 @@
-(function () {
-    var widgetIframe = document.getElementById('sc-widget');
-    var widget = SC.Widget(widgetIframe);
-    var songStart = 55000;
-    var songEnd = 65000;
-    var url2 = 'http://api.soundcloud.com/users/1539950/favorites';
+(function() {
+	var widgetIframe = document.getElementById('sc-widget');
+	var previewButton = document.querySelector('.preview');
+	var widget = SC.Widget(widgetIframe);
+	var songStart = parseFloat(previewButton.value);
+	var window = 3000;
+	var songEnd = songStart + window;
+	var url2 = 'http://api.soundcloud.com/users/1539950/favorites';
+	//var homeUrl = window.location;
 
+	// Template for adding event listeners.
+	function addEvent(element, eventName, callback) {
+		if (element.addEventListener) {
+			element.addEventListener(eventName, callback, false);
+		} else {
+			element.attachEvent(eventName, callback, false);
+		}
+	}
 
-    //Template for adding event listeners.
-    function addEvent(element, eventName, callback) {
-        if (element.addEventListener) {
-            element.addEventListener(eventName, callback, false);
-        } else {
-            element.attachEvent(eventName, callback, false);
-        }
-    }
+	// Adding the functionality of reloading the wiidget with a given url.
+	var reloadButton = document.querySelector('.reload');
+	var widgetUrlInput = document.querySelector('.urlInput');
+	addEvent(reloadButton, 'click', function() {
+		widget.load(widgetUrlInput.value, {
+			auto_play : true
+		});
+	});
 
-    //Adding the functionality of reloading the widget with a given url.
-    var reloadButton = document.querySelector('.reload');
-    var widgetUrlInput = document.querySelector('.urlInput');
-    addEvent(reloadButton, 'click', function () {
-        widget.load(widgetUrlInput.value, {
-            auto_play: true
-        });
-    });
+	//Adding the functionality of getTrack button with an id.
+	var getButton = document.querySelector('.getTrack');
+	var idInput = document.querySelector('.idInput');
+	addEvent(getButton, 'click', function() {
+		window.location.origin + idInput.value;
+	});
+	
+	// Adding functionality to the seek button with the given input.
+	var seekToButton = document.querySelector('.seekTo');
+	var seekInput = seekToButton.querySelector('input');
+	addEvent(seekToButton, 'click', function(e) {
+		// 'if' prevents the function to trigger when the input field is
+		// pressed.
+		if (e.target !== this) {
+			e.stopPropagation();
+			return false;
+		}
+		widget.seekTo(seekInput.value)
+	});
 
-    //Adding functionality to the seek button with the given input.
-    var seekToButton = document.querySelector('.seekTo');
-    var seekInput = seekToButton.querySelector('input');
-    addEvent(seekToButton, 'click', function (e) {
-        //'if' prevents the function to trigger when the input field is pressed.
-        if (e.target !== this) {
-            e.stopPropagation();
-            return false;
-        }
-        widget.seekTo(seekInput.value)
-    });
+	// Adding functionality to the 'next' button.
+	var reloadButton = document.querySelector('.next');
+	addEvent(reloadButton, 'click', function() {
+		widget.next();
+	});
 
-    //Adding functionality to the 'next' button.
-    var reloadButton = document.querySelector('.next');
-    addEvent(reloadButton, 'click', function () {
-        widget.next()
-    });
+	// Adding functionality to the 'prev' button.
+	var reloadButton = document.querySelector('.prev');
+	addEvent(reloadButton, 'click', function() {
+		widget.prev();
+	});
 
-    //Adding functionality to the 'prev' button.
-    var reloadButton = document.querySelector('.prev');
-    addEvent(reloadButton, 'click', function () {
-        widget.prev()
-    });
+	// Clear every event on the widget, used for debugging purposses.
+	var clearButton = document.querySelector('.clear');
+	addEvent(clearButton, 'click', function() {
+		widget.unbind(SC.Widget.Events.PLAY);
+		widget.unbind(SC.Widget.Events.READY);
+	});
 
-    // Adding functionality to the Preview button. Seeks to a certain point in the music
-    // and stops after a couple of seconds.
-    var previewButton = document.querySelector('.preview');
-    addEvent(previewButton, 'click', function (e) {
-        widget.bind(SC.Widget.Events.READY, function () {
-            if (widget.isPaused(function (paused) {
-                    if (paused) {
-                        widget.play();
-                    }
-                }))
-                widget.bind(SC.Widget.Events.PLAY, function () {
-                    widget.seekTo(songStart);
-                    widget.unbind(SC.Widget.Events.PLAY)
-                    widget.unbind(SC.Widget.Events.READY)
-                });
-        });
+	// Adding functionality to the Preview button. Seeks to a certain point in
+	// the music
+	// and stops after a couple of seconds.
 
-        widget.bind(SC.Widget.Events.PLAY_PROGRESS, function () {
-            widget.getPosition(
-                function (position) {
-                    if (position > songEnd) {
-                        widget.pause();
-                        widget.unbind(SC.Widget.Events.PLAY_PROGRESS)
-                    }
-                }
-            );
-        });
-    });
+	addEvent(previewButton, 'click', function(e) {
+		widget.bind(SC.Widget.Events.READY, function() {
+			if (widget.isPaused(function(paused) {
+				if (paused) {
+					widget.play();
+				}
+			}))
+				widget.bind(SC.Widget.Events.PLAY, function() {
+					widget.seekTo(songStart);
+					widget.unbind(SC.Widget.Events.PLAY);
+					widget.unbind(SC.Widget.Events.READY);
+				});
+		});
 
-    var randomSnippet = document.querySelector('.randSnip');
-    var randStart = randomSnippet.querySelector('value');
-    addEvent(randomSnippet, 'click', function (e) {
-        widget.bind(SC.Widget.Events.READY, function () {
-            if (widget.isPaused(function (paused) {
-                    if (paused) {
-                        widget.play();
-                    }
-                }))
-                widget.bind(SC.Widget.Events.PLAY, function () {
-                    widget.seekTo(randStart);
-                    widget.unbind(SC.Widget.Events.PLAY)
-                    widget.unbind(SC.Widget.Events.READY)
-                });
-        });
+		widget.bind(SC.Widget.Events.PLAY_PROGRESS, function() {
+			widget.getPosition(function(position) {
+				if (position > songEnd) {
+					widget.pause();
+					console.log(songStart)
+					console.log(songEnd)
+					widget.unbind(SC.Widget.Events.PLAY_PROGRESS);
+				} // else if ()
+			});
+		});
+	});
 
-        widget.bind(SC.Widget.Events.PLAY_PROGRESS, function () {
-            widget.getPosition(
-                function (position) {
-                    if (position > ( randStart + 3000 )) {
-                        widget.pause();
-                        widget.unbind(SC.Widget.Events.PLAY_PROGRESS)
-                    }
-                }
-            );
-        });
-    });
+	/*
+	 * var randomSnippet = document.querySelector('.randSnip');
+	 * addEvent(randomSnippet, 'click', function(e) {
+	 * widget.bind(SC.Widget.Events.READY, function() { if(widget.isPaused(
+	 * function(paused) { if( paused) { widget.play(); } } ))
+	 * widget.bind(SC.Widget.Events.PLAY,function(){ widget.seekTo(randStart);
+	 * widget.unbind(SC.Widget.Events.PLAY)
+	 * widget.unbind(SC.Widget.Events.READY) }); });
+	 * 
+	 * widget.bind(SC.Widget.Events.PLAY_PROGRESS, function() {
+	 * widget.getPosition( function(position) { if( position > randEnd) {
+	 * widget.pause(); widget.unbind(SC.Widget.Events.PLAY_PROGRESS) } } ); });
+	 * });
+	 */
 }());
